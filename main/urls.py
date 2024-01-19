02146a8fda57789a5a8b7cb3ca311ebcb5cea824
Rai Freire <rai.freire26@gmail.com>
2024-01-19 11:44:48 -0300

    add form aula04

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.alunoView, name='aluno-lista'),
+    path('alunoID/<int:id>', views.alunoIDview, name='aluno-detalhe'),
+    path('contact', views.contact_view, name='contact_view'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,8 +1,25 @@
 from .models import Aluno
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 
 def alunoView(request):
     alunos_list = Aluno.objects.all()
-    return render(request, 'main/alunos.html', {'alunos_list':alunos_list})
+    return render(request, 'main/alunos.html', {'alunos_list': alunos_list})
+
+
+def alunoIDview(request, id):
+    aluno = get_object_or_404(Aluno, pk=id)
+    print(aluno)
+    return render(request, 'main/alunoID.html', {'aluno': aluno})
+
+
+def contact_view(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print(name)
+        print(email)
+        print(message)
+    return render(request, 'main/contact.html')
